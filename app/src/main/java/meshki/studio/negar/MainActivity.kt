4a3c0af79ba7977ec613a50d7470597cb20faa10
Reza Hajianpour <hajianpour.mr@gmail.com>
2023-12-19 23:18:12 +0330

    add: koin
    Signed-off-by: Reza Hajianpour <hajianpour.mr@gmail.com>

--- app/src/main/java/meshki/studio/negar/MainActivity.kt
+++ app/src/main/java/meshki/studio/negar/MainActivity.kt
@@ -4,40 +4,48 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import meshki.studio.negar.ui.theme.NegarTheme
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+            val mainViewModel: MainViewModel by viewModel()
             NegarTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Scaffold(
+                    topBar = {  },
+                    bottomBar = {  },
+                ) { padding ->
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(padding),
+                        color = MaterialTheme.colorScheme.primary
+                    ) {
+                        Card(
+
+
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(top = 10.dp),
+                            elevation = CardDefaults.elevatedCardElevation()
+                        ) {
+                            Text(text = mainViewModel.isReady.value.toString())
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NegarTheme {
-        Greeting("Android")
     }
 }
