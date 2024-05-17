de2d7399ee47bc5464630b5fafc3090ba7e0e0ba
Reza Hajianpour <hajianpour.mr@gmail.com>
2024-05-18 01:09:19 +0330

    info: update version number
    Signed-off-by: Reza Hajianpour <hajianpour.mr@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "meshki.studio.negarname"
         minSdk = 21
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         resourceConfigurations.plus(listOf("en", "fa"))
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
