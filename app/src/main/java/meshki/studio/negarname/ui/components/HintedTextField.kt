0cfe6e57934451be1e84bebb30d1bdad380d9ff1
Reza Hajianpour <hajianpour.mr@gmail.com>
2024-07-04 11:30:32 +0330

    fix: add ime padding to edit notes

--- app/src/main/java/meshki/studio/negarname/ui/components/HintedTextField.kt
+++ app/src/main/java/meshki/studio/negarname/ui/components/HintedTextField.kt
@@ -1,20 +1,28 @@
 package meshki.studio.negarname.ui.components
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.selection.LocalTextSelectionColors
 import androidx.compose.foundation.text.selection.TextSelectionColors
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInRoot
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
@@ -44,8 +52,19 @@ fun HintedTextField(
         Box(
             modifier = modifier
         ) {
+            val scrollState = rememberScrollState()
+            var scrollToPosition by remember { mutableStateOf(0F) }
             val mod: Modifier = when (expanded) {
-                true -> { Modifier.fillMaxSize() }
+                true -> {
+                    Modifier
+                        .fillMaxSize()
+                        .imePadding()
+                        .onGloballyPositioned { layoutCoordinates ->
+                            scrollToPosition =
+                                scrollState.value + layoutCoordinates.positionInRoot().y
+                        }
+                        .verticalScroll(rememberScrollState())
+                }
                 false -> { Modifier.fillMaxWidth() }
             }
             BasicTextField(
